7a6dcaa9cff5ef35c8dfce70c4cef54390b20276
swapnilj081093 <swapnilj081093@gmail.com>
2024-06-11 17:06:09 +0530

    Work in Progress

--- swapniljgit/calculator_react/src/Components/layout.jsx
+++ swapniljgit/calculator_react/src/Components/layout.jsx
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const CenteredContainer = () => {
   const [display, setDisplay] = useState('');
@@ -29,24 +30,104 @@ const CenteredContainer = () => {
               <Row className="p-1">
                 
                 <Col xs={3} className="d-grid p-1" >
-                  <Button variant="outline-dark" onClick={handleButton}>-</Button>
-                </Col>
-
-                <Col xs={3} className="d-grid p-1" >
                   <Button variant="outline-dark" onClick={handleButton}>+</Button>
                 </Col>
 
                 <Col xs={3} className="d-grid p-1" >
-                  <Button variant="outline-dark" onClick={handleButton}>/</Button>
+                  <Button variant="outline-dark" onClick={handleButton}>-</Button>
                 </Col>
 
                 <Col xs={3} className="d-grid p-1" >
                   <Button variant="outline-dark" onClick={handleButton}>*</Button>
                 </Col>
+
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>/</Button>
+                </Col>
+                
+              </Row>
+
+              <Row className="p-1">
+                
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>7</Button>
+                </Col>
+
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>8</Button>
+                </Col>
+
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>9</Button>
+                </Col>
+
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>%</Button>
+                </Col>
                 
               </Row>
               
-              
+              <Row className="p-1">
+                
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>4</Button>
+                </Col>
+
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>5</Button>
+                </Col>
+
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>6</Button>
+                </Col>
+
+                <Col xs={3} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}><i className="bi bi-arrow-left"></i></Button>
+                </Col>
+                
+              </Row>
+
+              <Row>
+                <Col xs={9}>
+                <Row>
+
+                <Col xs={4} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>1</Button>
+                </Col>
+
+                <Col xs={4} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>2</Button>
+                </Col>
+
+                <Col xs={4} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>3</Button>
+                </Col>
+
+                <Col xs={4} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}>0</Button>
+                </Col>
+
+                <Col xs={4} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}><i className="bi bi-dot"></i></Button>
+                </Col>
+
+                <Col xs={4} className="d-grid p-1" >
+                  <Button variant="outline-dark" onClick={handleButton}><i className="bi bi-c-circle"></i></Button>
+                </Col>
+
+                </Row>
+                </Col>
+
+                <Col xs={3}>
+                <Row>
+                  <Col>
+                  <Button variant="outline-dark" onClick={handleButton}><i className="bi bi-c-circle"></i></Button>
+                  </Col>
+                </Row>
+
+                                
+                </Col>
+              </Row>
 
 
 
